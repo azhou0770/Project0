0db5d7b391b18984e72761016dfee8aabf59acd9
gmarte10 <giancarlosmarte10@gmail.com>
2024-08-02 14:18:58 -0400

    gmarte branch added

--- src/main/java/com/revature/model/Customer.java
+++ src/main/java/com/revature/model/Customer.java
@@ -1,4 +1,22 @@
 package com.revature.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id //makes this a primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="c_id",updatable = false)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
