8b0642369f360fb1df453abd2347251377c6e8d2
azhou0770 <azhou0770@gmail.com>
2024-08-05 11:05:21 -0700

    Implemented customer functionality, need to test it still though.

--- src/main/java/com/revature/app/StoreApplication.java
+++ src/main/java/com/revature/app/StoreApplication.java
@@ -2,10 +2,13 @@ package com.revature.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.revature")
+@EntityScan("com.revature.models")
+@EnableJpaRepositories("com.revature.repositories")
 public class StoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
 	}
--- src/main/java/com/revature/services/CustomerService.java
+++ src/main/java/com/revature/services/CustomerService.java
@@ -4,7 +4,7 @@ import com.revature.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public interface CustomerService {
-    public Customer addCustomer(Customer c);
-    public Customer getCustomer(String email, String password);
+    public Customer addCustomer(String name, String email, String password);
+    public Customer loginCustomer(String email, String password);
     public boolean deleteCustomer(String email, String password);
 }
