1df617dea7ab353c3c03033a247bd273468d22f8
azhou0770 <azhou0770@gmail.com>
2024-08-05 20:11:23 -0700

    Implemented POST storeowner/products

--- src/main/java/com/revature/services/StoreOwnerService.java
+++ src/main/java/com/revature/services/StoreOwnerService.java
@@ -1,11 +1,16 @@
 package com.revature.services;
 
+import com.revature.model.Product;
 import com.revature.model.StoreOwner;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface StoreOwnerService {
-    public StoreOwner createStoreOwner(StoreOwner c);
+    public StoreOwner createStoreOwner(StoreOwner so);
     public StoreOwner loginStoreOwner(String email, String password);
     public boolean deleteStoreOwner(String email, String password);
+
+    public List<Product> getProducts(String email, String password);
 }
--- src/main/java/com/revature/services/StoreOwnerServiceImpl.java
+++ src/main/java/com/revature/services/StoreOwnerServiceImpl.java
@@ -1,9 +1,12 @@
 package com.revature.services;
 
+import com.revature.model.Product;
 import com.revature.model.StoreOwner;
 import com.revature.repositories.StoreOwnerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StoreOwnerServiceImpl implements StoreOwnerService {
@@ -29,5 +32,11 @@ public class StoreOwnerServiceImpl implements StoreOwnerService {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public List<Product> getProducts(String email, String password){
+        StoreOwner storeOwner = sor.findByEmailAndPassword(email, password);
+        return storeOwner.getProducts();
     }
 }
